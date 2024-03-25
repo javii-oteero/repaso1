e80c86b175f285e9e4a9607086388a721ff68557
javi <javii.oteero@gmail.com>
2024-03-25 09:44:12 +0100

    Todos los tests creados y pasados

--- src/es/iesmz/tests/NumerosTest.java
+++ src/es/iesmz/tests/NumerosTest.java
@@ -8,4 +8,29 @@ class NumerosTest {
     void TestSonIgualesTresDistintos() {
         assertEquals(2, Numeros.sonIguales(4, 55, 7));
     }
+
+    @Test
+    void Test2SonIgualesTresDistintos() {
+        assertEquals(2, Numeros.sonIguales(14, 23, 27));
+    }
+
+    @Test
+    void TestSonIgualesDosIguales() {
+        assertEquals(1, Numeros.sonIguales(14,55,14));
+    }
+
+    @Test
+    void TestSonIgualesDosIguales2() {
+        assertEquals(1, Numeros.sonIguales(14,14,33));
+    }
+
+    @Test
+    void TestSonIgualesDosIguales3() {
+        assertEquals(1, Numeros.sonIguales(32,55,55));
+    }
+
+    @Test
+    void TestSonIgualesTodosIguales() {
+        assertEquals(0, Numeros.sonIguales(329,329,329));
+    }
 }
